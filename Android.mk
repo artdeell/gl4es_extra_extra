1748754c67b93ea85b65c1077c747415b7e1da05
Mathias-Boulay <mathiasboulay@free.fr>
2023-05-14 00:59:30 +0200

    Build: push compiler optimization to limits

--- Android.mk
+++ Android.mk
@@ -91,7 +91,7 @@ LOCAL_SRC_FILES := \
 	src/gl/vgpu/shaderconv.c \
         src/gl/iProtecc/iprotecc_string.c \
 
-LOCAL_CFLAGS += -g -std=gnu99 -funwind-tables -O3 -ffast-math -mllvm -polly -fvisibility=hidden -include include/android_debug.h
+LOCAL_CFLAGS += -g -std=gnu99 -funwind-tables -Ofast -ffast-math -mllvm -polly -flto -fvisibility=hidden -include include/android_debug.h
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
 #LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
